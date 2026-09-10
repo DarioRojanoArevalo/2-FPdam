3d97670e767c67de18fdc72c5cfbc811793a0d66
dario <dariorojano@gmail.com>
2026-09-11 00:51:37 +0200

    JDK 21 parece funcionar correctamente

--- ProgramacionMultimediaDispositivosMoviles/helloKotling/src/Main.kt
+++ ProgramacionMultimediaDispositivosMoviles/helloKotling/src/Main.kt
@@ -1,29 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    /*val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }*/
     helloKotling();
-   // varTypes();
+    varTypes();
 }
 
 fun helloKotling() {
     println("Hello from KOTLING, thas ist like java but shorter?")
-    print("the compilator translate to java machine and syso has chnaged by print but ln ist the same")
-}/*
+    println("the compilator translate to java machine and syso has chnaged by print but ln ist the same")
+}
 fun varTypes(){
     val a: Int=2;
-    print(a);
+    println(a);
     val b = a.toByte();
-    print(b);
+    println(b);
     val b2: Byte = 1 // OK, literals are checked statically
     println(b2)
 
@@ -33,4 +23,4 @@ fun varTypes(){
     val i2: String = b2.toString();
 
     val i3: Double = b2.toDouble();
-}*/
+}
